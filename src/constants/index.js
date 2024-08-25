9f0d710e2e268518d9187cabcc7f79a1f414fdc1
Sujith-33 <sujithsd02@gmail.com>
2024-08-25 20:14:37 +0530

    secomnd commit

---
+++ src/constants/index.js
@@ -0,0 +1,48 @@
+import project1 from "../assets/projects/project-1.png";
+import project2 from "../assets/projects/project-2.png";
+import project3 from "../assets/projects/project-3.png";
+import project4 from "../assets/projects/project-4.png";
+
+export const HERO_CONTENT = `As a Full Stack Developer, I possess expertise in both front-end and back-end development. I am proficient in creating responsive and dynamic user interfaces using HTML, CSS, and JavaScript, while also managing server-side operations, databases, and APIs with technologies like Node.js, Python, or PHP. My role involves designing, developing, and maintaining web applications from concept to deployment, ensuring seamless performance across all layers of the technology stack. I am a problem solver who bridges the gap between design and functionality, enabling a cohesive and efficient web development process.`;
+
+export const ABOUT_TEXT = `Motivated and technically proficient Full Stack Developer with expertise in HTML, CSS, Tailwind CSS, JavaScript, React, EJS,Express.js, Node.js, MongoDB, Java, Python, SQL, and C. Proven ability to design, develop, and deploy robust web applications. Experienced in building dynamic, responsive user interfaces and scalable backend systems. Adept at integrating
+
+modern technologies to create seamless user experiences. Strong problem-solving skills and a commitment to delivering high-
+quality code and efficient solutions.`;
+
+export const PROJECTS = [
+  {
+    title: "instaFeed",
+    image: project1,
+    description:
+      "Developed an Instagram clone. Features include secure  login/logout, a dynamic home feed, profile management with post uploads, and a user search functionality. Demonstrated expertise in full-stack development and integration of modern web technologies",
+    technologies: ["EJS", "Tailwind CSS", "Express", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Pintrest",
+    image: project2,
+    description:
+      "Developed a clone of the Pinterest website, featuring a responsive design. Implemented a search bar with dynamic suggestions based on the first letter typed, allowing users to quickly find images by name.",
+    technologies: ["HTML", "Tailwind CSS", "JavaScript"],
+  },
+  {
+    title: "Music Player",
+    image: project3,
+    description:
+      "A web-based music player .It features playback controls, including play, pause, forward, and backward functionality, providing a smooth and responsive user experience.",
+    technologies: ["HTML", "Tailwind CSS", "JavaScriptt"],
+  },
+  {
+    title: "E-commerce Analysis",
+    image: project4,
+    description:
+      "A comprehensive data analysis project. The analysis includes extracting, processing, and visualizing e-commerce data to uncover valuable insights and trends, aiding in business decision-making.",
+    technologies: ["Python", "Jupyter Notebook","mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "767 Fifth Avenue, New York, NY 10153 ",
+  phoneNo: "+91 9164172733",
+  email: "sujithsd02@gmail.com",
+};
